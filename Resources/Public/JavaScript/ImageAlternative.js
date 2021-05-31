949d5b9da3db894b130da2e6882194e3505fdf4f
Rune Piper <rune.piper@visuellverstehen.de>
2021-05-31 13:47:39 +0200

    Improve clientside validation

--- Resources/Public/JavaScript/ImageAlternative.js
+++ Resources/Public/JavaScript/ImageAlternative.js
@@ -1,33 +1,39 @@
 /**
- * Module: TYPO3/CMS/Hafenk/ImageAlternative
+ * Module: TYPO3/CMS/T3imagealternative/ImageAlternative
  */
 define([
     'jquery',
     'TYPO3/CMS/Backend/FormEngineValidation'
 ], function($, FormEngineValidation) {
-    var ImageAlternative = {
-        foo: 'bar'
-    };
+    var ImageAlternative = {};
 
     ImageAlternative.initialize = function() {
         $(document).find(FormEngineValidation.inputSelector).each(function() {
             if($(this).attr('data-formengine-validation-rules').indexOf('ImageAlternative') > -1) {
                 ImageAlternative.initializeInput(this);
-            }
 
-            $(document).on('change', FormEngineValidation.rulesSelector, function() {
-                ImageAlternative.initializeInput(this);
-            });
+                $(document).on('change', FormEngineValidation.rulesSelector, function() {
+                    ImageAlternative.initializeInput(this);
+                });
+            }
         });
     };
 
     ImageAlternative.initializeInput = function($element) {
-        if ($element.placeholder === '' && $element.value === '') {
-            $element.closest(FormEngineValidation.markerSelector).classList.toggle(FormEngineValidation.errorClass, true);
-            FormEngineValidation.markParentTab($($element), isValid);
+        var markParent = false;
 
-            $(document).trigger('t3-formengine-postfieldvalidation');
+        // Here we could do an actualy check for the sys_file_metadata table
+        // like in the evaluation class
+        if ($element.placeholder === '' && $element.value === '') {
+            var markParent = true;
         }
+
+        // Inspired by FormEngineValidation
+        const isValid = !markParent;
+        $element.closest(FormEngineValidation.markerSelector).classList.toggle(FormEngineValidation.errorClass, !isValid);
+        FormEngineValidation.markParentTab($($element), isValid);
+
+        $(document).trigger('t3-formengine-postfieldvalidation');
     };
 
     ImageAlternative.initialize();
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,5 +6,14 @@ defined('TYPO3_MODE') or die();
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\VV\T3imagealternative\Evaluation\ImageAlternative::class]
     = '';
 
+// Maybe use the $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['RequireJS']['postInitializationModules']
+// See PageRenderer->computeRequireJsConfig
 (\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class))
-    ->loadRequireJsModule('TYPO3/CMS/T3imagealternative/ImageAlternative');
+    ->addInlineSettingArray(
+        'RequireJS.PostInitializationModules',
+        [
+            'TYPO3/CMS/Backend/FormEngine' => [
+                'TYPO3/CMS/T3imagealternative/ImageAlternative'
+            ]
+        ]
+    );
