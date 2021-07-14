2a5a7c6a939b3baa1fcf29922caa0d60965620a0
vvtsch (vv0015) <timon.schoenfeld@visuellverstehen.de>
2021-07-14 14:54:17 +0200

    Add constraints to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,4 +10,9 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'alpha',
     'version' => '1.0.0',
     'clearCacheOnLoad' => 0,
+    'constraints' =>[
+        'depends' => [
+            'typo3' => '10.4.0 - 10.4.99'
+        ],
+    ]
 ];
